bd96f6486047a93caaf9fcb40cb7d945e066523d
banjos <brunoanjossilva@gmail.com>
2021-08-07 16:23:01 -0300

    add infra para start/stop do ec2

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "region" {
   description = "Define what region the instance will be deployed"
-  default = "us-east-1"
+  default     = "us-east-1"
 }
 
 variable "name" {
   description = "Name of the Application"
-  default = "hammer-ec2"
+  default     = "hammer-ec2"
 }
 
 variable "env" {
   description = "Environment of the Application"
-  default = "prod"
+  default     = "prod"
 }
 
 variable "ami" {
   description = "AWS AMI to be used "
-  default = "ami-07ebfd5b3428b6f4d"
+  default     = "ami-07ebfd5b3428b6f4d"
 }
 
 variable "instance_type" {
   description = "AWS Instance type defines the hardware configuration of the machine"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
